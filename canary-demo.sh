5fab952d008dac896ac664396cc6d7863d4e168d
Raimo Radczewski <hello@craftswerk.io>
2022-12-13 15:51:20 +0100

    wop

--- canary-demo.sh
+++ canary-demo.sh
@@ -3,11 +3,11 @@ while true
 do
   clear
   echo -n "Live:   "
-  curl https://hello.k3d.local.profitbricks.net/ ; echo
+  curl https://hello.k3d.localhost/ ; echo
   echo -n "Canary: "
-  curl https://hello-canary.k3d.local.profitbricks.net/ ; echo
+  curl https://hello-canary.k3d.localhost/ ; echo
   echo -n "Stable: "
-  curl https://hello-stable.k3d.local.profitbricks.net/ ; echo
+  curl https://hello-stable.k3d.localhost/ ; echo
   echo "-----------"
   sleep 1
 done
--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ yq -i \
 git add deployment.yaml
 git \
   -c user.name="Automated Deployment Script" \
-  -c user.email="noreply@k3d.local.profitbricks.net" \
+  -c user.email="noreply@k3d.localhost" \
   commit -m "Deploy example service ${IMAGE}"
 git pull --rebase --autostash
 git push
